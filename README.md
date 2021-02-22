d40ab130586b75af1a8613bfb1afa0c9f4bb1faa
Simon <simonbor@gmail.com>
2021-02-22 12:24:39 +0200

    readme and refactor

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-﻿
+﻿# Image Gallery 
+Pure JavaScript/.NET CORE Image Gallery
+
+## Solution run notes
+
+1. unzip solution folder 
+2. open *.sln file with VS2019 .NET 5.0
--- wwwroot/js/gallery.js
+++ wwwroot/js/gallery.js
@@ -1,37 +1,32 @@
 ﻿
 const initGallery = function() {
-    // Select the carousel you'll need to manipulate and the buttons you'll add events to
+    // carousel and the buttons to add events to
     const carousel = document.querySelector("[data-target='carousel']");
     const card = carousel.querySelector("[data-target='card']");
     const leftButton = document.querySelector("[data-action='slideLeft']");
     const rightButton = document.querySelector("[data-action='slideRight']");
 
-    // Prepare to limit the direction in which the carousel can slide, 
-    // and to control how much the carousel advances by each time.
-    // In order to slide the carousel so that only three cards are perfectly visible each time,
-    // you need to know the carousel width, and the margin placed on a given card in the carousel
+    // get the carousel width and the margin placed on a given card in the carousel
     const carouselWidth = carousel.offsetWidth;
     const cardStyle = card.currentStyle || window.getComputedStyle(card)
     const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
 
-    // Count the number of total cards you have
+    // Count the number of total cards
     const cardCount = carousel.querySelectorAll("[data-target='card']").length;
 
-    // Define an offset property to dynamically update by clicking the button controls
-    // as well as a maxX property so the carousel knows when to stop at the upper limit
+    // Define an offset property and maxX property so the carousel knows when to stop
     let offset = 0;
     const maxX = -((cardCount / 3) * carouselWidth +
         (cardMarginRight * (cardCount / 3)) -
         carouselWidth - cardMarginRight) + Number(cardStyle.width.match(/\d+/g)[0]);
 
-    // Add the click events
+    // arrow buttons events
     leftButton.addEventListener("click", function () {
         if (offset < 0) {
             offset += Number(cardStyle.width.match(/\d+/g)[0]) + cardMarginRight;
             carousel.style.transform = `translateX(${offset}px)`;
         }
     })
-
     rightButton.addEventListener("click", function () {
         if (offset > maxX) {
             offset -= Number(cardStyle.width.match(/\d+/g)[0]) + cardMarginRight;
@@ -78,7 +73,7 @@ const initThumbnails = async () => {
 
 const gallery = document.querySelector("[data-target='gallery']");
 let interval;
-const delay = 10;
+const delay = 30;
 
 gallery.addEventListener('mousemove', e => {
     clearInterval(interval);
