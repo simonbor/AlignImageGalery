08177adc526607b968b840af8ad273b2d91f621c
Simon <simonbor@gmail.com>
2021-02-25 15:18:28 +0200

    js refactor

--- wwwroot/js/start.js
+++ wwwroot/js/start.js
@@ -1,14 +1,16 @@
-﻿const delay = 30; // delay between reloads in sec
-const imgGallery = new imageGallery();
-let interval = setInterval(imgGallery.loadImages, 1000 * delay); // set circle gallery reload
+﻿(function () {
+    const delay = 30; // delay between reloads in sec
+    const imgGallery = new imageGallery();
+    let interval = setInterval(imgGallery.loadImages, 1000 * delay); // set circle gallery reload
 
-// set next reload only after idle of delay time (bonus)
-const gallery = document.querySelector("[data-target='gallery']");
-gallery.addEventListener('mousemove', e => {
-    clearInterval(interval);
-    interval = setInterval(imgGallery.loadImages, 1000 * delay);
-});
+    // set next reload only after idle of delay time (bonus)
+    const gallery = document.querySelector("[data-target='gallery']");
+    gallery.addEventListener('mousemove', e => {
+        clearInterval(interval);
+        interval = setInterval(imgGallery.loadImages, 1000 * delay);
+    });
 
-// create gallery and load images
-imgGallery.initGallery();
-imgGallery.loadImages();
+    // create gallery and load images
+    imgGallery.initGallery();
+    imgGallery.loadImages();
+})();
